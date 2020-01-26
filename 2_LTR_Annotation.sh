66eb5a3c4f9f977e115cacc25a9624d28d0ad582
rajewski <rajewski23@gmail.com>
2020-01-25 18:50:04 -0800

    change index

--- 2_LTR_Annotation.sh
+++ 2_LTR_Annotation.sh
@@ -5,31 +5,37 @@ module load hmmer/3.1b2
 
 #create an index of the fasta file you are going to search
 #only run this command if it hasnt been run before, to save time
-if [ ! -e NobtScaffolds.md5 ]; then
-    gt suffixerator -db NobtScaffolds.fa  -indexname NobtScaffolds -tis -suf -lcp -des -ssp -sds -dna -memlimit 1GB
+if [ ! -e index/scaffold584.md5 ]; then
+    echo Generating index for the scaffold...
+    mkdir -p index
+    gt suffixerator -db scaffold584.fasta  -indexname index/scaffold584 -tis -suf -lcp -des -ssp -sds -dna -memlimit 1GB
+    echo Done.
 fi
 
 #Run LTR Harvest to find putative LTR transposons
 #only run this command if it hasn't been run before, to save time
-if [ ! -e NobtScaffolds.ltrharvest.sorted.gff3 ]; then
+if [ ! -e scaffold584.ltrharvest.sorted.gff3 ]; then
+    echo Running LTRharvest to find putative LTR retrotransposons...
     mkdir -p outinner
     gt ltrharvest \
-	-index NobtScaffolds \
-	-gff3 NobtScaffolds.ltrharvest.gff3 \
-	-outinner outinner/NobtScaffolds.ltrharvest.outinner.fa \
+	-index index/scaffold584 \
+	-gff3 scaffold584.ltrharvest.gff3 \
+	-outinner outinner/scaffold584.ltrharvest.outinner.fa \
 	--seqids \
-	--maxdistltr 17000 \
-	-out NobtScaffolds.ltrharvest.fa > NobtScaffolds.ltrharvest.out
+	-out scaffold584.ltrharvest.fa > scaffold584.ltrharvest.out
     #Sort the outputted GFF3 file
-    gt gff3 -sort NobtScaffolds.ltrharvest.gff3 > NobtScaffolds.ltrharvest.sorted.gff3
+    gt gff3 -sort scaffold584.ltrharvest.gff3 > scaffold584.ltrharvest.sorted.gff3
+    echo Done.
 fi
 
 #Run LTR Digest to annotate protein/ORFS inside the LTR transposons
+echo Running LTRdigest to annotate features of the TEs...
 mkdir -p ltrdigest
 gt ltrdigest \
-    -outfileprefix ltrdigest/NobtScaffolds.ltrdigest \
+    -outfileprefix ltrdigest/scaffold584.ltrdigest \
     -trnas eukaryotic-tRNAs/eukaryotic-tRNAs.fa \
     -hmms gydb_hmms/GyDB_collection/profiles/*.hmm \
     --matchdesc \
-    --seqfile NobtScaffolds.fa \
-    -- NobtScaffolds.ltrharvest.sorted.gff3 NobtScaffolds > NobtScaffolds.ltrdigest.gff3
+    --seqfile scaffold584.fasta \
+    -- scaffold584.ltrharvest.sorted.gff3 index/scaffold584 > scaffold584.ltrdigest.gff3
+echo Done.
